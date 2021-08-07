b73cb328e896b0b2d7758f33b45199d5bfd8b014
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-08-07 17:51:50 +0100

    Added initial landing styles

--- src/components/landing/Landing.js
+++ src/components/landing/Landing.js
@@ -1,19 +1,22 @@
 import React, { Fragment } from 'react';
+import { Comet } from './Comet';
 
 const Landing = () => {
   return (
     <div className='title'>
-      <h1>
-        Welcome to my <span className='title-bar'>world</span>
-      </h1>
-      <h3>
-        You came to the right place to find out more about me, my interests and,
-        unexpectedly, about literature
-      </h3>
-      <button className='btn btn-projects'>
-        <p>See my projects</p>
-      </button>
-      {/* add comet */}
+      <div className='main'>
+        <h1>
+          Welcome to my <span className='title-bar'>world</span>
+        </h1>
+        <h3>
+          You came to the right place to find out more about me, my interests
+          and, unexpectedly, about literature
+        </h3>
+        <button className='btn btn-projects'>
+          <p>See my projects</p>
+        </button>
+      </div>
+      <Comet />
     </div>
   );
 };
