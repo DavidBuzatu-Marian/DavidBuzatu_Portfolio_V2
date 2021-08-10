a25588bb78c1008e04fdff31d36f48920d012d2e
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-08-10 19:46:49 +0100

    Finished hooks for blogs

--- src/components/landing_blog/hooks.js
+++ src/components/landing_blog/hooks.js
@@ -1,13 +1,9 @@
 import axios from 'axios';
 
 export const getLatestBlogs = async () => {
-  const token = process.env.REACT_APP_GITHUB_API;
-  axios.defaults.headers.common['Authorization'] = 'token ' + token;
-  const addBlogs = await axios.get(
-    'https://api.github.com/repos/DavidBuzatu-Marian/Blogs/contents'
-  );
+  const blogsFromRepository = await getBlogs();
   let requests_for_blog = [];
-  addBlogs.data.forEach((blog) => {
+  blogsFromRepository.data.forEach((blog) => {
     requests_for_blog.push(axios.get(blog.url));
   });
   const blogsPromises = await Promise.allSettled(requests_for_blog);
@@ -16,13 +12,40 @@ export const getLatestBlogs = async () => {
     let content = atob(blog.value.data.content);
     const splittedContentOnEnter = content.split('\n');
     blogs.push({
-      content,
+      content: getContent(splittedContentOnEnter),
       posted_at: getPostedAtDate(splittedContentOnEnter),
       blog_image: getBlogImage(splittedContentOnEnter),
       title: getBlogTitle(splittedContentOnEnter),
+      tags: getTags(splittedContentOnEnter),
     });
   });
+  blogs.sort((b1, b2) => {
+    return b2.posted_at - b1.posted_at;
+  });
   return blogs;
+};
+
+const getBlogs = async () => {
+  const token = process.env.REACT_APP_GITHUB_API;
+  axios.defaults.headers.common['Authorization'] = 'token ' + token;
+  return await axios.get(
+    'https://api.github.com/repos/DavidBuzatu-Marian/Blogs/contents'
+  );
+};
+
+const getTags = (splittedContentOnEnter) => {
+  return JSON.parse(eval(splittedContentOnEnter[4]));
+};
+
+const getContent = (splittedContentOnEnter) => {
+  const contentAfterTags = splittedContentOnEnter.slice(5);
+  let content = '';
+  contentAfterTags.forEach((line) => {
+    if (line !== '') {
+      content += line + '\n';
+    }
+  });
+  return content;
 };
 
 const getBlogTitle = (splittedContentOnEnter) => {
