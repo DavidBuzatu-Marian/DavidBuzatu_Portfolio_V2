a2f8ab1ad42273c288e65483d86686f63e86f731
davidbuzatu-marian <davidm.buzatu@gmail.com>
2021-08-10 08:58:46 +0100

    Added blog details

--- src/components/landing_blog/hooks.js
+++ src/components/landing_blog/hooks.js
@@ -14,13 +14,26 @@ export const getLatestBlogs = async () => {
   let blogs = [];
   blogsPromises.forEach((blog) => {
     let content = atob(blog.value.data.content);
-    blogs.push({ content, posted_at: getPostedAtDate(content) });
+    const splittedContentOnEnter = content.split('\n');
+    blogs.push({
+      content,
+      posted_at: getPostedAtDate(splittedContentOnEnter),
+      blog_image: getBlogImage(splittedContentOnEnter),
+      title: getBlogTitle(splittedContentOnEnter),
+    });
   });
   return blogs;
 };
 
-const getPostedAtDate = (content) => {
-  const splittedContentOnEnter = content.split('\n');
+const getBlogTitle = (splittedContentOnEnter) => {
+  return splittedContentOnEnter[3].substr(2); // have newline before for readability
+};
+
+const getBlogImage = (splittedContentOnEnter) => {
+  return splittedContentOnEnter[1];
+};
+
+const getPostedAtDate = (splittedContentOnEnter) => {
   const splittedPostedAtLine = splittedContentOnEnter[0].split(' ');
   const dateIndex = splittedPostedAtLine.length - 1;
   const dateInitialLength = splittedPostedAtLine[dateIndex].length;
